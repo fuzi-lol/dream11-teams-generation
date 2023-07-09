8efecf119faec6f09b04e9d4c0106eb44f24bc53
Naveen kumar dasari <62437753+fuzi-lol@users.noreply.github.com>
2023-07-09 11:48:58 +0530

    dream 11 team generation

---
+++ dream11.py
@@ -0,0 +1,563 @@
+#dream 11 dream teams.  */
+#players..
+#team1 = rohit[b] , bumrah[bo], decock[wk], s yadav[b],pattison[bo] , boult[bo],  h pandya[a],krunal pandya[a], pollard[a],chahar[bo], ishan kishan[wk].
+#team 2 = buttler[wk], smith[b],s gopal]bp]], archer[bo], tyagi[bo],rahul tewatia[a],lomoror[a],curran[bo],samson[wk], jaiswal[b],rajoot[bo]
+import random
+t1=['rohit','decock','syadav','ishanKishan','h pandya',' k pandya','pollard','chahar','pattison','boult','bumrah']
+t2=['buttler','smith','sgopal','archer','tyagi','Rtewatia','lomoror','curran','samson','jaiswal','rajoot']
+
+a=['h pandya','k pandya','pollard','lomoror','Rtewatia']
+b=['rohit','syadav','smith','jaiswal']
+wk=['buttler','samson','ishanKishan','decock']
+bo=['chahar','pattison','boult','bumrah','sgopal','archer','tyagi','curran','rajoot']
+credits=100.0
+creditpoints={'rohit':10.5,
+'decock':9.0,
+'syadav':9.0,
+'ishanKishan':8.5,
+'h pandya':9.5,
+'k pandya':8.5,
+'pollard':9.5,
+'chahar':8.0,
+'pattison':8.0,
+'boult':8.5,
+'bumrah':9.0,
+'buttler':10.0,
+'smith':10.0,
+'sgopal':8.5,
+'archer':9.0,
+'tyagi':8.0,
+'Rtewatia':8.5,
+'lomoror':8.0,
+'curran':8.5,
+'samson':9.5,
+'jaiswal':8.0,
+'rajoot':8.0
+}
+allteams=[]
+
+
+
+
+def two324():
+
+	
+	#for i in range(1):
+		t1count=0
+		t2count=0
+		team=[];
+		wkr=[]
+		wkr.extend(random.sample(wk,2))
+		team.extend(wkr)
+		bat=[]
+		bat.extend(random.sample(b,3))
+		team.extend(bat)
+		alr=[]
+		alr.extend(random.sample(a,2))
+		team.extend(alr)
+		
+		
+		bow=[]
+		bow.extend(random.sample(bo,4))
+		team.extend(bow)
+		teamcheck(team)
+					
+					
+				
+
+def two333():
+
+	#
+	#for i in range(1):
+		t1count=0
+		t2count=0
+		team=[];
+		wkr=[]
+		wkr.extend(random.sample(wk,2))
+		team.extend(wkr)
+		bat=[]
+		bat.extend(random.sample(b,3))
+		team.extend(bat)
+		alr=[]
+		alr.extend(random.sample(a,3))
+		team.extend(alr)
+		
+		
+		bow=[]
+		bow.extend(random.sample(bo,3))
+		team.extend(bow)
+		teamcheck(team)	
+def two315():
+
+	
+	#for i in range(1):
+		t1count=0
+		t2count=0
+		team=[];
+		wkr=[]
+		wkr.extend(random.sample(wk,2))
+		team.extend(wkr)
+		bat=[]
+		bat.extend(random.sample(b,3))
+		team.extend(bat)
+		alr=[]
+		alr.extend(random.sample(a,1))
+		team.extend(alr)
+		
+		
+		bow=[]
+		bow.extend(random.sample(bo,5))
+		team.extend(bow)
+		teamcheck(team)
+def two414():
+
+	
+	#for i in range(1):
+		t1count=0
+		t2count=0
+		team=[];
+		wkr=[]
+		wkr.extend(random.sample(wk,2))
+		team.extend(wkr)
+		bat=[]
+		bat.extend(random.sample(b,4))
+		team.extend(bat)
+		alr=[]
+		alr.extend(random.sample(a,1))
+		team.extend(alr)
+		
+		
+		bow=[]
+		bow.extend(random.sample(bo,4))
+		team.extend(bow)
+		teamcheck(team)
+def two423():
+
+	
+	#for i in range(1):
+		t1count=0
+		t2count=0
+		team=[];
+		wkr=[]
+		wkr.extend(random.sample(wk,2))
+		team.extend(wkr)
+		bat=[]
+		bat.extend(random.sample(b,4))
+		team.extend(bat)
+		alr=[]
+		alr.extend(random.sample(a,2))
+		team.extend(alr)
+		
+		
+		bow=[]
+		bow.extend(random.sample(bo,3))
+		team.extend(bow)
+		teamcheck(team)
+def two513():
+
+	#
+	#for i in range(1):
+		t1count=0
+		t2count=0
+		team=[];
+		wkr=[]
+		wkr.extend(random.sample(wk,2))
+		team.extend(wkr)
+		bat=[]
+		bat.extend(random.sample(b,5))
+		team.extend(bat)
+		alr=[]
+		alr.extend(random.sample(a,1))
+		team.extend(alr)
+		
+		
+		bow=[]
+		bow.extend(random.sample(bo,3))
+		team.extend(bow)
+		teamcheck(team)
+def three323():
+
+	
+	#for i in range(1):
+		t1count=0
+		t2count=0
+		team=[];
+		wkr=[]
+		wkr.extend(random.sample(wk,3))
+		team.extend(wkr)
+		bat=[]
+		bat.extend(random.sample(b,3))
+		team.extend(bat)
+		alr=[]
+		alr.extend(random.sample(a,2))
+		team.extend(alr)
+		
+		
+		bow=[]
+		bow.extend(random.sample(bo,3))
+		team.extend(bow)
+		teamcheck(team)
+def three314():
+
+	
+	#for i in range(1):
+		t1count=0
+		t2count=0
+		team=[];
+		wkr=[]
+		wkr.extend(random.sample(wk,3))
+		team.extend(wkr)
+		bat=[]
+		bat.extend(random.sample(b,3))
+		team.extend(bat)
+		alr=[]
+		alr.extend(random.sample(a,1))
+		team.extend(alr)
+		
+		
+		bow=[]
+		bow.extend(random.sample(bo,4))
+		team.extend(bow)
+		teamcheck(team)
+def three413():
+
+	
+	#for i in range(1):
+		t1count=0
+		t2count=0
+		team=[];
+		wkr=[]
+		wkr.extend(random.sample(wk,3))
+		team.extend(wkr)
+		bat=[]
+		bat.extend(random.sample(b,4))
+		team.extend(bat)
+		alr=[]
+		alr.extend(random.sample(a,1))
+		team.extend(alr)
+		
+		
+		bow=[]
+		bow.extend(random.sample(bo,3))
+		team.extend(bow)
+		teamcheck(team)
+					
+def four313():
+
+	
+	#for i in range(1):
+		t1count=0
+		t2count=0
+		team=[];
+		wkr=[]
+		wkr.extend(random.sample(wk,4))
+		team.extend(wkr)
+		bat=[]
+		bat.extend(random.sample(b,3))
+		team.extend(bat)
+		alr=[]
+		alr.extend(random.sample(a,1))
+		team.extend(alr)
+		
+		
+		bow=[]
+		bow.extend(random.sample(bo,3))
+		team.extend(bow)
+		teamcheck(team)
+					
+def one334():
+
+	#
+	#for i in range(1):
+		t1count=0
+		t2count=0
+		team=[];
+		wkr=[]
+		wkr.extend(random.sample(wk,1))
+		team.extend(wkr)
+		bat=[]
+		bat.extend(random.sample(b,3))
+		team.extend(bat)
+		alr=[]
+		alr.extend(random.sample(a,3))
+		team.extend(alr)
+		
+		
+		bow=[]
+		bow.extend(random.sample(bo,4))
+		team.extend(bow)
+		teamcheck(team)
+def one343():
+
+	
+	#for i in range(1):
+		t1count=0
+		t2count=0
+		team=[];
+		wkr=[]
+		wkr.extend(random.sample(wk,1))
+		team.extend(wkr)
+		bat=[]
+		bat.extend(random.sample(b,3))
+		team.extend(bat)
+		alr=[]
+		alr.extend(random.sample(a,4))
+		team.extend(alr)
+		
+		
+		bow=[]
+		bow.extend(random.sample(bo,3))
+		team.extend(bow)
+		teamcheck(team)
+def one325():
+
+	
+	#for i in range(1):
+		t1count=0
+		t2count=0
+		team=[];
+		wkr=[]
+		wkr.extend(random.sample(wk,1))
+		team.extend(wkr)
+		bat=[]
+		bat.extend(random.sample(b,3))
+		team.extend(bat)
+		alr=[]
+		alr.extend(random.sample(a,2))
+		team.extend(alr)
+		
+		
+		bow=[]
+		bow.extend(random.sample(bo,5))
+		team.extend(bow)
+		teamcheck(team)
+def one424():
+
+	
+	#for i in range(1):
+		t1count=0
+		t2count=0
+		team=[];
+		wkr=[]
+		wkr.extend(random.sample(wk,1))
+		team.extend(wkr)
+		bat=[]
+		bat.extend(random.sample(b,4))
+		team.extend(bat)
+		alr=[]
+		alr.extend(random.sample(a,2))
+		team.extend(alr)
+		
+		
+		bow=[]
+		bow.extend(random.sample(bo,4))
+		team.extend(bow)
+		teamcheck(team)
+def one433():
+
+	
+	#for i in range(1):
+		t1count=0
+		t2count=0
+		team=[];
+		wkr=[]
+		wkr.extend(random.sample(wk,1))
+		team.extend(wkr)
+		bat=[]
+		bat.extend(random.sample(b,4))
+		team.extend(bat)
+		alr=[]
+		alr.extend(random.sample(a,3))
+		team.extend(alr)
+		
+		
+		bow=[]
+		bow.extend(random.sample(bo,3))
+		team.extend(bow)
+		teamcheck(team)
+def one523():
+
+	
+	#for i in range(1):
+		
+		team=[];
+		wkr=[]
+		wkr.extend(random.sample(wk,1))
+		team.extend(wkr)
+		bat=[]
+		bat.extend(random.sample(b,5))
+		team.extend(bat)
+		alr=[]
+		alr.extend(random.sample(a,2))
+		team.extend(alr)
+		
+		
+		bow=[]
+		bow.extend(random.sample(bo,3))
+		team.extend(bow)
+		teamcheck(team)
+		
+					
+def one514():
+
+	
+	#for i in range(1):
+		t1count=0
+		t2count=0
+		team=[];
+		wkr=[]
+		wkr.extend(random.sample(wk,1))
+		team.extend(wkr)
+		bat=[]
+		bat.extend(random.sample(b,5))
+		team.extend(bat)
+		alr=[]
+		alr.extend(random.sample(a,1))
+		team.extend(alr)
+		
+		
+		bow=[]
+		bow.extend(random.sample(bo,4))
+		team.extend(bow)
+		teamcheck(team)
+					
+def teamcheck(team):
+	t1count=0
+	t2count=0
+	for i in team:
+			if i in t1:
+				t1count+=1
+			if i in t2:
+				t2count+=1
+	if (t1count<=7 and t1count>=4) or (t2count<=7 and t2count>=4):
+			totalcredits=0
+			for i in team:
+				totalcredits+=creditpoints[i]
+			if totalcredits<=credits:
+				
+				if team not in allteams:
+					allteams.append(team)
+					#print(team)
+	
+'''#one334()
+one343()
+one325()
+one424()
+one433()
+#one523()
+#one514()
+two333()
+two315()
+two324()
+two414()
+two423()
+#two513()
+three323()
+three314()
+three413()
+#four313()'''
+
+
+def allteamsprint():
+	print("\n\n")
+	for i in allteams:
+		aa=allteams.index(i)
+		print("********** TEAM : "+str(aa)+ " *********")
+		for j in i:
+			print(j)
+	print("\n")
+
+#taking choice checking conditions to print wk orders
+choice=int(input("enter choice wk 1,2,3,4 : "))
+howmanyteams=int(input("enter how many teams u want to generate : "))
+
+if choice==1:
+	i=0
+	while i<howmanyteams:
+	
+		try:
+			one334()
+			howmanyteams-=1
+		except:
+			pass
+		try:
+			one343()
+			howmanyteams-=1
+		except:
+			pass
+		try:	
+			one325()
+			howmanyteams-=1
+		except:
+			pass
+		try:
+			one424()
+			howmanyteams-=1
+		except:
+			pass
+		try:
+			one433() 
+			howmanyteams-=1
+		except:
+			pass
+		try:
+			one523()
+			howmanyteams-=1
+			one514()
+			howmanyteams-=1
+		except:
+			pass
+		i+=1
+elif choice==2:
+	i=0
+	while i<howmanyteams:
+		try:
+			two333()
+			howmanyteams-=1
+		except:
+			pass
+		try:		
+			two315()
+			howmanyteams-=1
+		except:
+			pass
+		two324()
+		howmanyteams-=1
+		two414()
+		howmanyteams-=1
+		two423()
+		howmanyteams-=1
+		try:
+			two513()
+			howmanyteams-=1
+		except:
+			pass
+elif choice ==3:
+	i=0
+	while i<howmanyteams:
+		try:
+			three323()
+			howmanyteams-=1
+			three314()
+			howmanyteams-=1
+			three413()
+			howmanyteams-=1
+		except:
+			pass
+elif choice ==4:
+	i=0
+	while i<howmanyteams:
+		try: 
+			four313()
+			howmanyteams-=1
+		except:
+			pass
+allteamsprint()
+	
+
+
+
+
+
+
+
